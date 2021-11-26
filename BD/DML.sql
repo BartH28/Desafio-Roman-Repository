d78bbc17b200b08e83c875c531b8e44ab31151bd
unknown <48967937814>
2021-11-26 14:38:00 -0300

    Banco de Dados

---
+++ BD/DML.sql
@@ -0,0 +1,15 @@
+USE Roman_g12_t
+GO
+
+INSERT INTO USUARIO (nome, email, senha)
+VALUES ('Kleber','kleber@gmail.com','kleber123'),('Klaudio','klaudio@gmail.com','klaudio123'),('Klauber','klauber@gmail.com','klauber123');
+GO
+
+INSERT INTO TEMA (nomeTema)
+VALUES ('Banco de Dados'),('ReactJS'),('React Native'),('Front-End'),('Back-End'),('Design'),('UX/UI');
+GO
+
+INSERT INTO PROJETO (idTema, idUsuario, nome, descricao)
+VALUES 
+(2,1,'Projeto BD','Projeto visando ensinar todos os conceitos por trás de um banco de dados utilizando o Sql Server'),
+(5,3,'Projeto Cavalo','Projeto visando modelar um cavalo utilizando o emulador do android studio');
