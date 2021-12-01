56e34d9e86ab7d3177c84b785fe22b2cfaa0c88d
unknown <50742583830>
2021-12-01 17:09:36 -0300

    tela de cadastro terminada

--- Front-End/Roman/screens/cadastro.js
+++ Front-End/Roman/screens/cadastro.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import RNPickerSelect from 'react-native-picker-select';
+// import RNPickerSelect from 'react-native-picker-select';
+import {Picker} from '@react-native-picker/picker';
 import React, { Component } from 'react';
 import {
 
@@ -11,10 +12,11 @@ import {
     View,
     ScrollView,
     SectionList,
-    Picker,
     TouchableOpacity
 } from 'react-native';
 import api from '../Services/api';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class Cadastro extends Component {
     constructor(props) {
@@ -22,18 +24,21 @@ export default class Cadastro extends Component {
         this.state = {
           nome: '',
           idTema:0,
-          listaTema:[1, 2, 3, 4,5,6,7],
+        //   listaTema:[1, 2, 3, 4,5,6,7],
+        itemValue: 0,
           descricao: ''
         };
       }
 
 
-      cadastrar = async projeto =>
+      cadastrar = async ()  =>
       {
         try{
-            const evento = await {
+
+
+            const projeto = {
                     nome: this.state.nome,
-                    idTema: this.state.idTema,
+                    idTema: this.state.itemValue,
                     descricao: this.state.descricao
             }
 
@@ -42,7 +47,7 @@ export default class Cadastro extends Component {
 
             
 
-            const resp = await api.post('/Projetos', evento,{
+            const resp = await api.post('/Projetos', projeto,{
                 headers: {
                   Authorization: 'Bearer ' + token,
                 }
@@ -50,57 +55,63 @@ export default class Cadastro extends Component {
 
             if (resp.status === 201) {
                 this.props.navigation.navigate('Projeto')
+                this.setState({nome:''})
             }
 
-        }catch (erro){
-            console.warn(error)
+        }catch (error){
             throw error
         }
       }
 
+      realizarLogout = async () => {
+        try {
+          await AsyncStorage.removeItem('TokenU');
+          this.props.navigation.navigate('Login'); 
+        } catch (error) {
+          console.warn(error);
+        }
+      };
+
 
     render() {
         return (
-            <View>
-                <Text>Cadastro</Text>
-                <View>
-                    <TextInput style={styles.inputLogin}
+            <View >
+            <TouchableOpacity
+           style={styles.btnSair}
+            onPress={this.realizarLogout}>
+            <Text  >sair</Text>
+          </TouchableOpacity>
+
+                <Text style={styles.TextoC}>Cadastro</Text>
+                <View style={styles.tudo}>
+                    <TextInput style={styles.inputnome}
                         placeholder="Nome Projeto"
                         placeholderTextColor="rgba(0,0,0,0.65)"
                         keyboardType="default"
                         onChangeText={nome => this.setState({ nome })}
+                        value={this.state.nome}
 
                     ></TextInput>
-                    
-                    {/* <Picker
-                        selectedValue={selectedValue}
-                        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                    >
-                        <Picker.Item label="Banco De Dados" value={1} />
-                        <Picker.Item label="ReactJS" value={2} />
-                        <Picker.Item label="React Native" value={3} />
-                        <Picker.Item label="Front-End" value={4} />
-                        <Picker.Item label="Back-End" value={5} />
-                        <Picker.Item label="Design" value={6} />
-                        <Picker.Item label="Ux/Ui" value={7} />
 
-                    </Picker> */}
-
-                    <RNPickerSelect
-                                onValueChange={(  idTema  ) => this.setState(idTema)}
-                                items={[
-                                    { label: 'Banco De Dados', value: 1 },
-                                    { label: 'ReactJS', value: 2 },
-                                    { label: 'React Native', value: 3 },
-                                    { label: 'Front-End', value: 4 },
-                                    { label: 'Back-End', value: 5 },
-                                    { label: 'Design', value: 6 },
-                                    { label: 'Ux/Ui', value: 7 },
-                                ]}
-                            />
+                    <Picker
+                            style={styles.select}
+                            selectedValue={this.state.itemValue}
+                            style={{ height: 60, width: 327}}
+                            onValueChange={(itemValue) => this.setState({itemValue})}
+                        >
+                            <Picker.Item label="Selecione um Tema" value={null} />
+                            <Picker.Item label="Banco de dados" value={1} />
+                            <Picker.Item label="ReactJS" value={2} />
+                            <Picker.Item label="React Native" value={3} />
+                            <Picker.Item label="Front-End" value={4} />
+                            <Picker.Item label="Back-End" value={5} />
+                            <Picker.Item label="Design" value={6} />
+                            <Picker.Item label="Ux/Ui" value={7} />
+                        </Picker>
 
 
-                    <TextInput style={styles.inputLogin}
+
+                    <TextInput  style={styles.inputnome}
                         placeholder="Descrição"
                         placeholderTextColor="rgba(0,0,0,0.65)"
                         keyboardType="default"
@@ -129,5 +140,67 @@ const styles = StyleSheet.create({
     main: {
       flex: 1
    
-    }
+    },
+
+    tudo: { 
+        
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(39,198,250,0.27)',
+        borderRadius:20,
+        width:'90%',
+        height:'50%',
+        marginLeft:20,
+        flexDirection:'column'
+
+     },
+    
+
+    TextoC:{
+        fontSize:36,
+        marginTop:80,
+        marginBottom:80,
+        alignItems:'center',
+        marginLeft:130,
+        fontFamily: 'Rosarivo-Regular',
+        
+        },
+
+    inputnome:{
+     width:300,
+     borderBottomColor: 'rgba(39,198,250,0.66)',
+     borderBottomWidth: 2
+
+    },
+    btnCadastro:{
+        width:80,
+        height:30,
+        backgroundColor: 'rgba(39,198,250,0.66)',
+        borderRadius:50,
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:20
+        },
+       
+        btnText:{
+         color:"black"
+        },
+
+        select:{
+            borderBottomColor: 'rgba(39,198,250,0.66)',
+            borderBottomWidth: 2  
+        },
+
+        btnSair:{
+            width:71,
+            height:22,
+            backgroundColor: 'rgba(39,198,250,0.66)',
+            borderRadius:50,
+            alignItems:'center',
+            justifyContent:'center',
+            marginTop:20,
+            marginLeft:24
+            
+            },
+
   });
--- Front-End/Roman/screens/listaProj.js
+++ Front-End/Roman/screens/listaProj.js
@@ -1,136 +1,202 @@
 import React, { Component } from 'react';
 import {
 
-  StyleSheet,
-  Text,
-  Image,
-  TextInput,
-  FlatList,
-  View,
-  ScrollView,
-  SectionList
+    StyleSheet,
+    Text,
+    Image,
+    TextInput,
+    FlatList,
+    View,
+    ScrollView,
+    SectionList
 } from 'react-native';
 
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../Services/api';
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 
 export default class Projetos extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             listaProjetos: []
         }
     }
 
     buscarProj = async () => {
         const token = await AsyncStorage.getItem('TokenU');
-       
 
-        const resp = await api.get('/Projetos',{
+
+        const resp = await api.get('/Projetos', {
             headers: {
-              Authorization: 'Bearer ' + token,
+                Authorization: 'Bearer ' + token,
             },
-          }
-          );
-        this.setState({listaProjetos: resp.data})
+        }
+        );
+        this.setState({ listaProjetos: resp.data })
     };
 
-    componentDidMount(){
+    componentDidMount() {
         this.buscarProj();
 
     }
 
-    render(){
-        return(
+    realizarLogout = async () => {
+        try {
+            await AsyncStorage.removeItem('TokenU');
+            this.props.navigation.navigate('Login');
+        } catch (error) {
+            console.warn(error);
+        }
+    };
+
+    render() {
+        return (
+
+            <View >
 
 
-         
-    //     <ScrollView
-    //      contentContainerStyle={styles.container}
-    //      >
-    //         <Text style={styles.textoP}>Projetos</Text>
-    //         <Image source={require('../assets/img/menina.png')}
-    //       style={styles.imgmenina}
-    //    />
-                <View >
-                    <FlatList
-                        ListHeaderComponent={
-                            <>
+
+                <FlatList
+
+
+                    ListHeaderComponent={
+                        <>
+                        <View  style={styles.container_btn}>
+                            <TouchableOpacity
+                                style={styles.btnSair}
+                                onPress={this.realizarLogout}>
+                                <Text style={styles.btnLogoutText}>sair</Text>
+                            </TouchableOpacity>
+                        </View>
+                            <TouchableOpacity
+                                style={styles.btnCadastro}
+                                onPress={this.buscarProj}
+                            >
+                                <View style={styles.b}>
+
+                                    <Text style={styles.btnText}>
+                                        Atualizar
+                                    </Text>
+                                    <Image style={styles.refreshIcon} source={require('../assets/img/refreshIcon.png')} />
+                                </View>
+
+
+                            </TouchableOpacity>
                             <Text style={styles.textoP}>Projetos</Text>
                             <Image source={require('../assets/img/menina.png')}
-                            style={styles.imgmenina}/>
-                            </>
-                        }
-                        contentContainerStyle={styles.container}
-                        data={this.state.listaProjetos}
-                        keyExtractor={item => item.idProjeto}
-                        renderItem={this.renderItem}
-                    />
+                                style={styles.imgmenina} />
+                        </>
+                    }
+                    ListFooterComponent={
+                        <>
+                        
+                        </>
 
-                </View>
-        // </ScrollView>
-    )  
+                    }
+                    contentContainerStyle={styles.container}
+                    data={this.state.listaProjetos}
+                    keyExtractor={item => item.idProjeto}
+                    renderItem={this.renderItem}
+
+                />
+
+            </View>
+            // </ScrollView>
+        )
     }
 
-    renderItem = ({item}) => (
-       
-     <View style={styles.quadrado}>
-         <Text style={styles.nomeP}>{item.nome}</Text>
-         <Text style={styles.nomeTema}>{item.idTemaNavigation.nomeTema}</Text>
-         <Text style={styles.descricao}>{item.descricao}</Text>
-     </View>
+    renderItem = ({ item }) => (
+
+        <View style={styles.quadrado}>
+            <Text style={styles.nomeP}>{item.nome}</Text>
+            <Text style={styles.nomeTema}>{item.idTemaNavigation.nomeTema}</Text>
+            <Text style={styles.descricao}>{item.descricao}</Text>
+        </View>
     )
 
 }
 
 const styles = StyleSheet.create({
 
-    container:{
-        alignItems:'center',
-        backgroundColor: 'rgba(39,198,250,0.05)'
-    },
-
-    textoP:{
-    fontSize:36,
-    marginTop:80,
-    marginBottom:40,
-    marginLeft:30
-    },
-
-    imgmenina:{
-        marginBottom:50
-    },
-
-    quadrado:{
-        borderColor:'rgba(39,198,250,100)',
-        borderWidth:1,
-        width:319,
-        height:200,
-        borderRadius:20,
-        alignItems:'center',
-        marginBottom:55
+    container: {
+        alignItems: 'center',
+        backgroundColor: 'rgba(39,198,250,0.05)',
         
     },
-    nomeP:{
-        fontSize:18,
-        borderBottomWidth:1,
-        borderBottomColor:"rgba(39,198,250,100)",
-        paddingTop:13,
-        paddingBottom:8,
+
+    textoP: {
+        fontSize: 36,
+        marginTop: 40,
+        marginBottom: 40,
+        marginLeft: 45,
+        fontFamily: 'Rosarivo-Regular',
     },
 
-    nomeTema:{
-        fontSize:16,
-        paddingTop:20
+    imgmenina: {
+        marginBottom: 50,
+        marginLeft: 20
+
     },
 
-    descricao:{
-        width:270,
-        paddingTop:20
+    quadrado: {
+        borderColor: 'rgba(39,198,250,100)',
+        borderWidth: 1,
+        width: 319,
+        height: 200,
+        borderRadius: 20,
+        alignItems: 'center',
+        marginBottom: 55
+
+    },
+    nomeP: {
+        fontSize: 18,
+        borderBottomWidth: 1,
+        borderBottomColor: "rgba(39,198,250,100)",
+        paddingTop: 13,
+        paddingBottom: 8,
+        fontFamily: 'Rosarivo-Regular',
+
+    },
+
+    nomeTema: {
+        fontSize: 16,
+        paddingTop: 20
+    },
+
+    descricao: {
+        width: 270,
+        paddingTop: 20
+    },
+    b: {
+        width: 90,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 50,
+        backgroundColor: 'rgba(39,198,250,0.32)',
+        marginLeft: 70,
+        borderRadius: 20
+
+    },
+    refreshIcon: {
+        marginTop: 3,
+        marginLeft: 5
+    },
+    btnSair: {
+        width: 71,
+        height: 22,
+        backgroundColor: 'rgba(39,198,250,0.66)',
+        borderRadius: 50,
+        alignItems: 'center',
+        // justifyContent: 'center',
+        marginTop: 20,
+    },
+    container_btn:{
+       
     }
-   
+
 
 
 
